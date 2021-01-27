d3ade508176bbaeaa797832cddeb64ce24a1a9fe
raghav1010 <49784384+raghav1010@users.noreply.github.com>
2021-01-27 22:27:54 +0530

    Delete app.py

--- app.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask
-from flask_restful import Api
-from customers import Customer
-app = Flask(__name__)
-api = Api(app)
-api.add_resource(Customer, '/customers')
-
-if __name__ == '__main__':
-    app.run(debug=True)
