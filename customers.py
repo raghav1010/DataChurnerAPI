9c1525d2707917e222ad27d6aff1dae37b8edef7
raghav1010 <49784384+raghav1010@users.noreply.github.com>
2021-01-27 15:52:22 +0530

    Add files via upload

--- customers.py
+++ customers.py
@@ -7,9 +7,14 @@ class Customer(Resource):
 
     def get(self):
         di={}
-        if request.args:
+        x={"CLIENTNUM","Attrition_Flag","Customer_Age","Gender","Dependent_count","Education_Level","Marital_Status","Income_Category","Card_Category","Months_on_book","Total_Relationship_Count","Months_Inactive_12_mon","Contacts_Count_12_mon","Credit_Limit","Total_Revolving_Bal","Avg_Open_To_Buy","Total_Amt_Chng_Q4_Q1","Total_Trans_Amt", "Total_Trans_Ct", "Total_Ct_Chng_Q4_Q1" ,"Avg_Utilization_Ratio" , "Naive_Bayes_Classifier_Attrition_Flag_Card_Category_Contacts_Count_12_mon_Dependent_count_Education_Level_Months_Inactive_12_mon_1", "Naive_Bayes_Classifier_Attrition_Flag_Card_Category_Contacts_Count_12_mon_Dependent_count_Education_Level_Months_Inactive_12_mon_2"}
+        if request.args :
+
             for i in request.args.items():
-                di[i[0]]=i[1]
+                if i[0] in x:
+                    di[i[0]]=i[1]
+                else:
+                    return {'message': 'Please enter a valid parameter query'}, 400
 
         item = self.find_by_cusid(di)
         if item:
